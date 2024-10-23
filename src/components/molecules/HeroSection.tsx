5f787f0cb61434fcb7bd14bf2af724639c282327
dejosemario <josemariaodumodu@gmail.com>
2024-10-23 16:32:42 +0100

    refactor(component): move code to new avatar component

--- src/components/molecules/HeroSection.tsx
+++ src/components/molecules/HeroSection.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -9,7 +8,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
-import myImage from "@/public/myImage.png";
+import Avatar from "../atoms/Avatar";
 
 export default function HeroSection() {
   const { ref } = useSectionInView("Home", 0.5);
@@ -21,58 +20,18 @@ export default function HeroSection() {
       id="home"
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
-      <div className="flex items-center justify-center">
-        <div className="relative">
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "tween",
-              duration: 0.2,
-            }}
-          >
-            <Image
-              src={myImage}
-              alt="My Image"
-              width="192"
-              height="192"
-              quality="95"
-              priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
-            />
-          </motion.div>
-
-          <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 125,
-              delay: 0.1,
-              duration: 0.7,
-            }}
-          >
-            👋
-          </motion.span>
-        </div>
-      </div>
-
+      <Avatar />
       <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Odumodu Josemaria.</span> I'm a{" "}
-        <span className="font-bold">
-          full-stack developer,
-        </span>{" "}
-        focused more on the backend with <span className="font-bold">3 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="font-bold">
-        Node (Nest.js) and React (Next.js)
-        </span>
-        .
+        <span className="font-bold">full-stack developer,</span> focused more on
+        the backend with <span className="font-bold">3 years</span> of
+        experience. I enjoy building{" "}
+        <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="font-bold">Node (Nest.js) and React (Next.js)</span>.
       </motion.h1>
 
       <motion.div
