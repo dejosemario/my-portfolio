6a2b75dcdd381e106ace4c762f271b4453182b87
dejosemario <josemariaodumodu@gmail.com>
2024-10-24 15:10:38 +0100

    chore: add the created components to it's parent component/page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import SectionDivider from '@/components/atoms/SectionDivider'
+import About from '@/components/molecules/About'
+import HeroSection from '@/components/molecules/HeroSection'
+import Projects from '@/components/organisms/Projects'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>My portfolio</div>
+    <main className="flex min-h-screen flex-col items-center justify-between px-4">
+    <HeroSection />
+    <SectionDivider />
+    <About />
+    <Projects />
     </main>
   )
 }
