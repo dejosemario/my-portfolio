d9ab1166952c78ffedf626b48676b8e6bada2d62
dejosemario <josemariaodumodu@gmail.com>
2024-11-04 23:52:55 +0100

    feat: created the skills component section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import SectionDivider from '@/components/atoms/SectionDivider'
 import About from '@/components/molecules/About'
 import HeroSection from '@/components/molecules/HeroSection'
+import Skills from '@/components/molecules/skills'
 import Projects from '@/components/organisms/Projects'
 import Image from 'next/image'
 
@@ -10,7 +11,11 @@ export default function Home() {
     <HeroSection />
     <SectionDivider />
     <About />
+    <SectionDivider />
+
     <Projects />
+    <Skills />
+
     </main>
   )
 }
