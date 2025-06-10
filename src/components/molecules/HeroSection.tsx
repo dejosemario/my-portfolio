3133adcd6165d67c945aeb43da8049671bb14247
Dejosemario <josemariaodumodu@gmail.com>
2025-06-10 16:17:21 +0100

    fix: update CV download link attributes for improved accessibility

--- src/components/molecules/HeroSection.tsx
+++ src/components/molecules/HeroSection.tsx
@@ -57,7 +57,9 @@ export default function HeroSection() {
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/CV.pdf"
-          download
+          download="Odumodu_Josemaria_CV.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Download CV{"  "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
