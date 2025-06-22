d334886cc02a18bc49924761fcfd1ba1c8780b16
Dejosemario <josemariaodumodu@gmail.com>
2025-06-22 22:00:45 +0100

    fix: update CV download link to point to the correct file

--- src/components/molecules/HeroSection.tsx
+++ src/components/molecules/HeroSection.tsx
@@ -56,7 +56,7 @@ export default function HeroSection() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/josemaria.pdf"
+          href="/Josemaria_Odumodu_CV.pdf"
           download="Odumodu_Josemaria_CV.pdf"
           target="_blank"
           rel="noopener noreferrer"
